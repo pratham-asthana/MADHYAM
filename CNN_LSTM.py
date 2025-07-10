ba3f10b47ea60b6e68acb27988ada7770ca56368
Pratham Asthana <83860600+pratham-asthana@users.noreply.github.com>
2025-07-10 13:49:13 +0530

    Update CNN_LSTM.py

--- CNN_LSTM.py
+++ CNN_LSTM.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, LSTM, Dense, TimeDistributed, Flatten, Dropout
 
-# Define the CNN feature extractor
+# Defining the CNN feature extractor
 def create_feature_extractor(input_shape):
     model = Sequential([
         Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
@@ -12,20 +12,19 @@ def create_feature_extractor(input_shape):
     ])
     return model
 
-# Define the hybrid CNN-LSTM model
-input_shape = (64, 64, 3)  # Input frame shape
-sequence_length = 30  # Number of frames per sequence
+input_shape = (64, 64, 3)  
+sequence_length = 30  
 
 feature_extractor = create_feature_extractor(input_shape)
 
 model = Sequential([
     TimeDistributed(feature_extractor, input_shape=(sequence_length, *input_shape)),
     LSTM(64, return_sequences=True),
-    Dropout(0.2),  # Dropout layer added
+    Dropout(0.2), 
     LSTM(64),
-    Dropout(0.2),  # Dropout layer added
+    Dropout(0.2),  
     Dense(128, activation='relu'),
-    Dense(47, activation='softmax')  # 47 classes
+    Dense(47, activation='softmax') 
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
